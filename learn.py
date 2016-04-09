ff970badd653165dc10d87c228792d1263ceb9c7
Aron Granberg <aron.granberg@gmail.com>
2016-04-09 17:55:52 +0200

    Add checkpoints

--- learn.py
+++ learn.py
@@ -8,16 +8,16 @@ input_size = pixels
 input_channels = 3
 output_size = pixels
 
-x = tf.placeholder(tf.float32, [None, input_size*input_channels])
-y_ = tf.placeholder(tf.float32, [None, output_size])
+x = tf.placeholder(tf.float32, [None, input_size*input_channels], name="x")
+y_ = tf.placeholder(tf.float32, [None, output_size], name="y_")
 
-def weigth_var(shape):
+def weigth_var(shape, name):
 	initial = tf.truncated_normal(shape, stddev=0.1)
-	return tf.Variable(initial)
+	return tf.Variable(initial, name=name)
 
-def bias_var(shape):
+def bias_var(shape, name):
 	initial = tf.constant(0.1, shape=shape)
-	return tf.Variable(initial)
+	return tf.Variable(initial, name=name)
 
 def conv2d(x, W):
 	return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
@@ -27,20 +27,20 @@ def max_pool_2x2(x):
 
 
 
-W_conv1 = weigth_var([5, 5, input_channels, 64])
-b_conv1 = bias_var([64])
+W_conv1 = weigth_var([5, 5, input_channels, 64], "W_conv1")
+b_conv1 = bias_var([64], "b_conv1")
 x_image = tf.reshape(x, [-1, size, size, input_channels])
 
 # Shape [?, size, size, 32]
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 
 
-W_conv2 = weigth_var([5, 5, 64, 32])
-b_conv2 = bias_var([32])
+W_conv2 = weigth_var([5, 5, 64, 32], "W_conv2")
+b_conv2 = bias_var([32], "b_conv2")
 h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
 
-W_conv3 = weigth_var([5, 5, 32, 8])
-b_conv3 = bias_var([8])
+W_conv3 = weigth_var([5, 5, 32, 8], "W_conv3")
+b_conv3 = bias_var([8], "b_conv3")
 h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3) + b_conv3)
 
 # h_pool2 = max_pool_2x2(h_conv2)
@@ -54,8 +54,8 @@ h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3) + b_conv3)
 # keep_prob = tf.placeholder(tf.float32)
 # h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-W_conv4 = weigth_var([5, 5, 8, 1])
-b_conv4 = bias_var([1])
+W_conv4 = weigth_var([5, 5, 8, 1], "W_conv4")
+b_conv4 = bias_var([1], "b_conv4")
 h_conv4 = tf.nn.relu(conv2d(h_conv3, W_conv4) + b_conv4)
 
 h_conv4_flat = tf.reshape(h_conv4, [-1, size*size])
@@ -74,12 +74,18 @@ y = tf.nn.softmax(h_conv4_flat)
 cross_entropy = -tf.reduce_sum(y_*tf.log(y))
 train_step = tf.train.AdamOptimizer(0.002).minimize(cross_entropy)
 #train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-# correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
-# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+#correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
+#accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+saver = tf.train.Saver()
+save_every = 100
 
 init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
+
+if input("Restore from checkpoint? [y/N]").strip() == "y":
+	saver.restore(sess, "checkpoints/checkpoint-300")
 
 for i in range(50000):
 	batch_size = 200
@@ -97,6 +103,10 @@ for i in range(50000):
 		loss = sess.run(cross_entropy, feed_dict={x: batch_xs, y_: batch_ys}) / batch_size
 		print("Step: {0} Epoch: {1:.3f} Loss: {2:.2f}".format(i, goinput.epoch(), loss))
 		t6 = time.clock()
+
+	if i % save_every == 0:
+		print("Saving checkpoint...")
+		print(saver.save(sess, "checkpoints/checkpoint", global_step=i))
 
 	t3 = time.clock()
 	sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
